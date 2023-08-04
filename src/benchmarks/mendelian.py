2a2ebe77dd00535b249ea0464300f154e4f01f70
SamYu0310 <shy1@rice.edu>
2023-08-04 00:37:22 -0700

    added commands to store output of bcftools view of merged file

--- src/benchmarks/mendelian.py
+++ src/benchmarks/mendelian.py
@@ -1,8 +1,11 @@
 import subprocess
 import re 
+import os
+import time
 
 class mendelian: 
-    def __init__(self, current, new, unqiue_id, snf_ids): 
+    def __init__(self, plugins, current, new, unqiue_id, snf_ids): 
+        self.plugins = plugins
         self.current = current
         self.new = new
         self.unique_id = unqiue_id
@@ -10,6 +13,7 @@ class mendelian:
     
     def run(self): 
          # Run both versions of sniffles when merging the snf triplets 
+        job_ids_command1 = []  # To store the job IDs for command1
         for number in range(0, 2): 
             snf_version = "current_snf" if number == 0 else "new_snf"
             snf_path = self.current if number == 0 else self.new
@@ -39,8 +43,36 @@ class mendelian:
             job_id1 = ""
             if match:
                 job_id1 = match.group()
-                job_ids.append(job_id1)
+                job_ids_command1.append(job_id1)
             else:
                 raise ValueError("Failed to extract job ID for command1.")
+            
+            # Wait for the jobs sent by command1 to complete
+            for job in job_ids_command1:
+                while True:
+                    check_status_command1 = f'scontrol show job {job}'
+                    process_status1 = subprocess.run(check_status_command1, shell=True, capture_output=True, text=True)
+                    status_output1 = process_status1.stdout
+
+                    if 'JobState=COMPLETED' in status_output1:
+                        break  # Job completed, move to the next job_id1
+
+                    time.sleep(10)  # Wait for 10 seconds before checking again
+
+            # Set the BCFTOOLS_PLUGINS environment variable
+            os.environ["BCFTOOLS_PLUGINS"] = f"{self.plugins}"
+
+            # Command to run bcftools +mendelian2
+            mendelian_command = f'bcftools +mendelian2 {snf_version}merge{self.unique_id}_output.vcf.gz -p \
+            {snf_version}_MOTHER{self.unique_id}output,{snf_version}_FATHER{self.unique_id},{snf_version}_PROBAND{self.unique_id}'
+
+
+            # Run the mendelian command and capture the output
+            completed_process = subprocess.run(mendelian_command, shell=True, check=True, capture_output=True, text=True)
+            output = completed_process.stdout
+
+            # Write the output to a file
+            with open(f'mendelian{self.unique_id}_output.txt', 'w') as output_file:
+                output_file.write(output)
 
         return 0
